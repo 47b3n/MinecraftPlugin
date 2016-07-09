97fe595e40d210871ad0b68f52edd3297c1d6d27
Ruben de Groot <rpg.degroot@gmail.com>
2016-07-09 13:32:31 +0200

    Read description
    When first line of sign equals 47b3n, and player rightclicks sign the
    player gets teleported to somewhere

--- src/_47b3n/plugin/events/player/PlayerInteract.java
+++ src/_47b3n/plugin/events/player/PlayerInteract.java
@@ -1,12 +1,15 @@
 package _47b3n.plugin.events.player;
 
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import _47b3n.plugin.Main;
+import net.md_5.bungee.api.ChatColor;
 
 public class PlayerInteract implements Listener {
 	
@@ -20,10 +23,14 @@ public class PlayerInteract implements Listener {
 	public void onInteract(PlayerInteractEvent e) {
 		if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
 			if(e.getClickedBlock().getState() instanceof Sign) {
-				@SuppressWarnings("unused")
 				Sign sign = (Sign) e.getClickedBlock().getState();
+				String valueSign = sign.getLine(0);
 				
-				e.getPlayer().sendMessage("You right clicked a sign!");
+				if(valueSign.equalsIgnoreCase(plugin.getConfig().getString("Warp"))) {
+					e.getPlayer().teleport(new Location(e.getPlayer().getWorld(), 20, 64, 10)); 
+				}
+				
+				e.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "You right clicked a sign!");
 			}
 		}
 	}
