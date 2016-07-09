a950de2477c2226ce934623d9efc14ff5e0fd585
Ruben de Groot <rpg.degroot@gmail.com>
2016-07-09 13:37:04 +0200

    Deleted a unused import

--- src/_47b3n/plugin/events/player/PlayerInteract.java
+++ src/_47b3n/plugin/events/player/PlayerInteract.java
@@ -2,7 +2,6 @@ package _47b3n.plugin.events.player;
 
 import org.bukkit.Location;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
